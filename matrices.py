73947f1bb52c58054b21ae9ad21520de9fcdbd70
tomasvinuela <102112914+tomasvinuela@users.noreply.github.com>
2022-04-22 15:02:11 -0300

    Delete matrices.py

--- matrices.py
+++
@@ -1,5 +0,0 @@
-matriz = [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
-# se arma una lista o vector con otras listas o vectores dentro suyo, para asi generar una matriz. Cada vector sera una fila y cada elemento una columna
-for i in range(len(matriz)):
-    for j in range(len(matriz[i])):
-        print(matriz[i][j])
