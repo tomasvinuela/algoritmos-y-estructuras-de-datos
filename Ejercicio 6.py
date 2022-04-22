1677b34490e65aaf1a76881ffeb6d06f73fde3db
tomasvinuela <102112914+tomasvinuela@users.noreply.github.com>
2022-04-22 15:02:42 -0300

    Delete Ejercicio 6.py

--- Ejercicio 6.py
+++
@@ -1,6 +0,0 @@
-def invert_cadena(cadena, indice):
-    if (indice == 1):
-        return cadena[indice-1]
-    else:
-        return cadena[indice-1] + invert_cadena(cadena, indice-1)
-print(invert_cadena("hola", len("hola")))
