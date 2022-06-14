895ea208c42857d4f8c2e6991ca552b83c417f89
tomasvinuela <102112914+tomasvinuela@users.noreply.github.com>
2022-06-13 23:27:04 -0300

    Add files via upload

--- Parcial 1/Parcial 1.py
+++ Parcial 1/Parcial 1.py
@@ -1,31 +1,4 @@
-#Consigna 1: Organizar el archivo, mostrar los datos y encontrar cual dinosaurio fue el ultimo en ser descubierto y por quien
 
-#Consigna 2: Procesar el archivo .txt, organizarlo en 2 estructuras 1 por fecha y 1 por nombre,
-#Agregar el nombre de los dinosaurios de acuerdo a su numero
-
-#Consigna 3: Eliminar la informacion de las zonas WYG075, SXH966 y LYF010. Modificar la zona
-#HYD195 y cambiar el nombre a Mosasaurus.
-
-#Consigna 4: Generar un listado que solo incluya los dinosaurios: Tyrannosaurus Rex, Spinosaurus, Giganotosaurus con
-#nivel  ́critical’ o ‘high’.
-
-#Consigna 5: Tomar toda la información de alertas e insertarlas en 2 colas, una con los datos de dinosaurios
-#carnívoros y otra con los herbívoros, descartando los de nivel ‘low’ y ‘medium’
-
-#Consigna 6: Mostrar las alertas de la cola de carnivoros, descartando las de la zona EPC944
-
-#Consigna 7: Enviar toda la informacion de los Raptors y Carnotaurus, y los códigos de las zonas 
-#donde hay dinosaurios Compsognathus.
-
-#Consigna 8: desde que Jurassic Park arranco la calve a ha sido ‘mosquito’ pero que
-# significa esto realmente, si lo consideramos como si fueran números estas serian las situaciones:
-# 1. si el número está entre 33 y 47 su valor alfanumérico esta ok.
-# 2. caso contrario
-# si número {es divisible por 3 entonces (número // 2) + 9 (es tu nuevo valor alfanumérico)
-#           {sino número -14 (es tu nuevo valor alfanumérico)
-# en cualquiera de los casos debes continuar procesandolo, es una solución parcial.
-# 3. al final obtendrás la clave si sabes cómo hacer las cosas, pero recuerda ‘mosquito’ es la clave
-# de todo.
 
 #Informacion adicional:
 # module_name: jurassic_park.py
@@ -44,20 +17,6 @@ from clasescola import Cola
 from random import randint, choice
 
 
-class Dinosaurio_Alerta:
-    def __init__(self, hora, zona, dinonumber, nivel, nombre, tipo, periodo, nombradopor):
-        self.hora = hora
-        self.zona = zona
-        self.dinonumber = dinonumber
-        self.nivel = nivel
-        self.nombre = nombre
-        self.tipo = tipo
-        self.periodo = periodo
-        self.nombreadopor = nombradopor
-
-    def __str__(self):
-        return f"{self.hora} | {self.zona} | {self.dinonumber} | { self.nivel}"
-
 class Alerta:
     def __init__(self, hora, zona, dinonumber, nivel, name):
         self.hora = hora
@@ -65,6 +24,10 @@ class Alerta:
         self.dinonumber = dinonumber
         self.nivel = nivel
         self.name = name
+
+    def __str__(self):
+        return f"{self.hora} | {self.zona} | {self.dinonumber} | { self.nivel} | { self.name}"
+
 
 class Dinosaurio:
     def __init__(self, name, type, number, period, named_by, date):
@@ -74,6 +37,10 @@ class Dinosaurio:
         self.period = period
         self.named_by = named_by
         self.date = date
+    
+    
+    def __str__(self):
+        return f"{self.name} | {self.type} | {self.number} | { self.period} | { self.named_by} | { self.date}"
 
 
 #Consigna 1: Organizar el archivo, mostrar los datos y encontrar cual dinosaurio fue el ultimo en ser descubierto y por quien
@@ -81,7 +48,7 @@ class Dinosaurio:
 listadinos = Lista()
 for dino in dinosaurs:
     auxiliar = dino['named_by'].split(',')
-    listadinos.insertar(Dinosaurio(dino['name'], dino['type'], dino['number'], dino['period'], auxiliar[0], auxiliar[1]), 'name')
+    listadinos.insertar(Dinosaurio(dino['name'], dino['type'], dino['number'], dino['period'], auxiliar[0], auxiliar[1]), 'number')
 
 print(listadinos.mayor_de_lista('date'))
 
@@ -99,13 +66,17 @@ for linea in lineas:
     datos = linea.split(';')        
                  
     print(datos)
-    din = listadinos.busqueda(datos[2], 'number')
+    for dino in dinosaurs:
+        if(dino['number'] == int(datos[2])):
+            nombree = dino['name']
+
+
     lista_alerta_fecha.insertar(Alerta(datos[0], 
                         datos[1], 
-                        datos[2], datos[3], din.name()), 'hora')
+                        datos[2], datos[3], nombree), 'hora')
     lista_alerta_nombre.insertar(Alerta(datos[0], 
                         datos[1], 
-                        datos[2], datos[3], din.name()), 'name')
+                        datos[2], datos[3], nombree), 'name')
 
 #Consigna 3: Eliminar la informacion de las zonas WYG075, SXH966 y LYF010. Modificar la zona
 #HYD195 y cambiar el nombre a Mosasaurus.
@@ -113,16 +84,17 @@ for linea in lineas:
 lista_alerta_fecha.eliminar('WYG075', 'zona')
 lista_alerta_fecha.eliminar('SXH966', 'zona')
 lista_alerta_fecha.eliminar('LYF010', 'zona')
+
 auxia = lista_alerta_fecha.eliminar('HYD195', 'zona')
-auxia['name'] = 'Mosasaurus'
-lista_alerta_fecha.insertar(auxia)
+auxia2 = Alerta(auxia['hora'], auxia['zona'], auxia['dinonumber'], auxia['nivel'], 'Mosasaurus')
+lista_alerta_fecha.insertar(auxia2)
 
 lista_alerta_nombre.eliminar('WYG075', 'zona')
 lista_alerta_nombre.eliminar('SXH966', 'zona')
 lista_alerta_nombre.eliminar('LYF010', 'zona')
 auxia = lista_alerta_nombre.eliminar('HYD195', 'zona')
-auxia['name'] = 'Mosasaurus'
-lista_alerta_nombre.insertar(auxia)
+auxia2 = Alerta(auxia['hora'], auxia['zona'], auxia['dinonumber'], auxia['nivel'], 'Mosasaurus')
+lista_alerta_nombre.insertar(auxia2)
 
 #Consigna 4: Generar un listado que solo incluya los dinosaurios: Tyrannosaurus Rex, Spinosaurus, Giganotosaurus con
 #nivel  ́critical’ o ‘high’.
@@ -133,11 +105,11 @@ contador = 0
 while(contador < lista_alerta_fecha.tamanio()):
     auxxx = lista_alerta_fecha.obtener_elemento(contador)
     auxxx2 = listadinos.busqueda(auxxx['name'])
-    if (auxxx2.info.type == 'Tyrannosaurus Rex') and (auxxx.nivel is 'critical' or 'high'):
+    if (auxxx2.info.type is 'Tyrannosaurus Rex') and ('critical' or 'high' in auxxx.nivel):
         lista_critical_high.insertar(auxxx)
-    if (auxxx2.info.type == 'Spinosaurus') and (auxxx.nivel is 'critical' or 'high'):
+    if (auxxx2.info.type is 'Spinosaurus') and ('critical' or 'high' in auxxx.nivel):
         lista_critical_high.insertar(auxxx)    
-    if (auxxx2.info.type == 'Giganotosaurus') and (auxxx.nivel is 'critical' or 'high'):
+    if (auxxx2.info.type is 'Giganotosaurus') and ('critical' or 'high' in auxxx.nivel):
         lista_critical_high.insertar(auxxx)
     contador += 1
 
@@ -152,9 +124,9 @@ contador = 0
 while(contador < lista_alerta_fecha.tamanio()):
     auxxx = lista_alerta_fecha.obtener_elemento(contador)
     auxxx2 = listadinos.busqueda(auxxx['name'])
-    if (auxxx2.info.type == 'carnivoro') and (auxxx.nivel is not 'low' or 'medium'):
+    if (auxxx2.info.type == 'carnivoro') and not ('low' or 'medium' in auxxx.nivel):
         cola_carnivoros.arribo(auxxx)
-    if (auxxx2.info.type == 'hervivoro') and (auxxx.nivel is not 'low' or 'medium'):
+    if (auxxx2.info.type == 'hervivoro') and not ('low' or 'medium' in auxxx.nivel):
         cola_hervivoros.arribo(auxxx)
     contador += 1
 
