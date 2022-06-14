d1b07af4d5500131b6314c99361c9f7763c05002
tomasvinuela <102112914+tomasvinuela@users.noreply.github.com>
2022-06-13 21:09:53 -0300

    Add files via upload

---
+++ Parcial 1/Parcial 1.py
@@ -0,0 +1,212 @@
+#Consigna 1: Organizar el archivo, mostrar los datos y encontrar cual dinosaurio fue el ultimo en ser descubierto y por quien
+
+#Consigna 2: Procesar el archivo .txt, organizarlo en 2 estructuras 1 por fecha y 1 por nombre,
+#Agregar el nombre de los dinosaurios de acuerdo a su numero
+
+#Consigna 3: Eliminar la informacion de las zonas WYG075, SXH966 y LYF010. Modificar la zona
+#HYD195 y cambiar el nombre a Mosasaurus.
+
+#Consigna 4: Generar un listado que solo incluya los dinosaurios: Tyrannosaurus Rex, Spinosaurus, Giganotosaurus con
+#nivel  ́critical’ o ‘high’.
+
+#Consigna 5: Tomar toda la información de alertas e insertarlas en 2 colas, una con los datos de dinosaurios
+#carnívoros y otra con los herbívoros, descartando los de nivel ‘low’ y ‘medium’
+
+#Consigna 6: Mostrar las alertas de la cola de carnivoros, descartando las de la zona EPC944
+
+#Consigna 7: Enviar toda la informacion de los Raptors y Carnotaurus, y los códigos de las zonas 
+#donde hay dinosaurios Compsognathus.
+
+#Consigna 8: desde que Jurassic Park arranco la calve a ha sido ‘mosquito’ pero que
+# significa esto realmente, si lo consideramos como si fueran números estas serian las situaciones:
+# 1. si el número está entre 33 y 47 su valor alfanumérico esta ok.
+# 2. caso contrario
+# si número {es divisible por 3 entonces (número // 2) + 9 (es tu nuevo valor alfanumérico)
+#           {sino número -14 (es tu nuevo valor alfanumérico)
+# en cualquiera de los casos debes continuar procesandolo, es una solución parcial.
+# 3. al final obtendrás la clave si sabes cómo hacer las cosas, pero recuerda ‘mosquito’ es la clave
+# de todo.
+
+#Informacion adicional:
+# module_name: jurassic_park.py
+# Puedes hacer lo siguiente:
+# from jurassic_park import dinosaurs
+# Devuelve una lista de diccionarios con información de todos los dinosaurios del parque con el
+# siguiente formato: {'name': ..., 'type': ..., 'number': ..., 'period': ..., 'named_by': ...}
+# Esta información te puede servir de ayuda para complementar los datos del archivo.
+
+# El archivo alerts.txt tiene la siguiente info (time, zone_code, dino_number, alert_level) por
+# columnas separados por ‘;’.
+
+from jurassic_park import dinosaurs
+from claseslistaparcial import Lista
+from clasescola import Cola
+from random import randint, choice
+
+
+class Dinosaurio_Alerta:
+    def __init__(self, hora, zona, dinonumber, nivel, nombre, tipo, periodo, nombradopor):
+        self.hora = hora
+        self.zona = zona
+        self.dinonumber = dinonumber
+        self.nivel = nivel
+        self.nombre = nombre
+        self.tipo = tipo
+        self.periodo = periodo
+        self.nombreadopor = nombradopor
+
+    def __str__(self):
+        return f"{self.hora} | {self.zona} | {self.dinonumber} | { self.nivel}"
+
+class Alerta:
+    def __init__(self, hora, zona, dinonumber, nivel, name):
+        self.hora = hora
+        self.zona = zona
+        self.dinonumber = dinonumber
+        self.nivel = nivel
+        self.name = name
+
+class Dinosaurio:
+    def __init__(self, name, type, number, period, named_by, date):
+        self.name = name
+        self.type = type
+        self.number = number
+        self.period = period
+        self.named_by = named_by
+        self.date = date
+
+
+#Consigna 1: Organizar el archivo, mostrar los datos y encontrar cual dinosaurio fue el ultimo en ser descubierto y por quien
+
+listadinos = Lista()
+for dino in dinosaurs:
+    auxiliar = dino['named_by'].split(',')
+    listadinos.insertar(Dinosaurio(dino['name'], dino['type'], dino['number'], dino['period'], auxiliar[0], auxiliar[1]), 'name')
+
+print(listadinos.mayor_de_lista('date'))
+
+#Consigna 2: Procesar el archivo .txt, organizarlo en 2 estructuras 1 por fecha y 1 por nombre,
+#Agregar el nombre de los dinosaurios de acuerdo a su numero
+
+file = open('alerts.txt')
+lineas = file.readlines()           
+lista_alerta_fecha = Lista()
+lista_alerta_nombre = Lista()
+lineas.pop(0)  
+
+
+for linea in lineas:
+    datos = linea.split(';')        
+                 
+    print(datos)
+    din = listadinos.busqueda(datos[2], 'number')
+    lista_alerta_fecha.insertar(Alerta(datos[0], 
+                        datos[1], 
+                        datos[2], datos[3], din.name()), 'hora')
+    lista_alerta_nombre.insertar(Alerta(datos[0], 
+                        datos[1], 
+                        datos[2], datos[3], din.name()), 'name')
+
+#Consigna 3: Eliminar la informacion de las zonas WYG075, SXH966 y LYF010. Modificar la zona
+#HYD195 y cambiar el nombre a Mosasaurus.
+
+lista_alerta_fecha.eliminar('WYG075', 'zona')
+lista_alerta_fecha.eliminar('SXH966', 'zona')
+lista_alerta_fecha.eliminar('LYF010', 'zona')
+auxia = lista_alerta_fecha.eliminar('HYD195', 'zona')
+auxia['name'] = 'Mosasaurus'
+lista_alerta_fecha.insertar(auxia)
+
+lista_alerta_nombre.eliminar('WYG075', 'zona')
+lista_alerta_nombre.eliminar('SXH966', 'zona')
+lista_alerta_nombre.eliminar('LYF010', 'zona')
+auxia = lista_alerta_nombre.eliminar('HYD195', 'zona')
+auxia['name'] = 'Mosasaurus'
+lista_alerta_nombre.insertar(auxia)
+
+#Consigna 4: Generar un listado que solo incluya los dinosaurios: Tyrannosaurus Rex, Spinosaurus, Giganotosaurus con
+#nivel  ́critical’ o ‘high’.
+
+lista_critical_high = Lista()
+contador = 0
+
+while(contador < lista_alerta_fecha.tamanio()):
+    auxxx = lista_alerta_fecha.obtener_elemento(contador)
+    auxxx2 = listadinos.busqueda(auxxx['name'])
+    if (auxxx2.info.type == 'Tyrannosaurus Rex') and (auxxx.nivel is 'critical' or 'high'):
+        lista_critical_high.insertar(auxxx)
+    if (auxxx2.info.type == 'Spinosaurus') and (auxxx.nivel is 'critical' or 'high'):
+        lista_critical_high.insertar(auxxx)    
+    if (auxxx2.info.type == 'Giganotosaurus') and (auxxx.nivel is 'critical' or 'high'):
+        lista_critical_high.insertar(auxxx)
+    contador += 1
+
+
+#Consigna 5: Tomar toda la información de alertas e insertarlas en 2 colas, una con los datos de dinosaurios
+#carnívoros y otra con los herbívoros, descartando los de nivel ‘low’ y ‘medium’
+
+cola_carnivoros = Cola()
+cola_hervivoros = Cola()
+
+contador = 0
+while(contador < lista_alerta_fecha.tamanio()):
+    auxxx = lista_alerta_fecha.obtener_elemento(contador)
+    auxxx2 = listadinos.busqueda(auxxx['name'])
+    if (auxxx2.info.type == 'carnivoro') and (auxxx.nivel is not 'low' or 'medium'):
+        cola_carnivoros.arribo(auxxx)
+    if (auxxx2.info.type == 'hervivoro') and (auxxx.nivel is not 'low' or 'medium'):
+        cola_hervivoros.arribo(auxxx)
+    contador += 1
+
+#Consigna 6: Mostrar las alertas de la cola de carnivoros, descartando las de la zona EPC944
+
+contador = 0
+while(contador < cola_carnivoros.tamanio):
+    auxilia = cola_carnivoros.en_frente()
+    if(auxilia['zona'] is not 'EPC944'):
+        print(auxilia)
+    cola_carnivoros.mover_al_final()
+    contador += 1
+cola_carnivoros.mover_al_final()
+
+#Consigna 7: Enviar toda la informacion de los Raptors y Carnotaurus, y los códigos de las zonas 
+#donde hay dinosaurios Compsognathus.
+
+listadinos.barrido_segun_dino('Raptor')
+listadinos.barrido_segun_dino('Carnotaurus')
+lista_alerta_nombre.barrido_segun_dino_zona('Compsognathus')
+
+
+#Consigna 8: desde que Jurassic Park arranco la calve a ha sido ‘mosquito’ pero que
+# significa esto realmente, si lo consideramos como si fueran números estas serian las situaciones:
+# 1. si el número está entre 33 y 47 su valor alfanumérico esta ok.
+# 2. caso contrario
+# si número {es divisible por 3 entonces (número // 2) + 9 (es tu nuevo valor alfanumérico)
+#           {sino número -14 (es tu nuevo valor alfanumérico)
+# en cualquiera de los casos debes continuar procesandolo, es una solución parcial.
+# 3. al final obtendrás la clave si sabes cómo hacer las cosas, pero recuerda ‘mosquito’ es la clave
+# de todo.
+
+clave = 'mosquito'
+cuenta = 0
+resolucion = 0
+while (cuenta <= 7):
+    aux3 = int(clave[cuenta])
+    if(aux3 >= 33 and aux3 <= 47):
+        if (aux3 % 3 == 0):
+            aux3 = ((aux3 // 2) + 9)
+        else:
+            aux3 = -14
+    resolucion += aux3
+    cuenta += 1
+
+print('La contraseña es: ' + resolucion)
+
+
+
+
+
+
+
+ 
+             
---
+++ Parcial 1/claseslistaparcial.py
@@ -0,0 +1,174 @@
+
+def criterio(dato, campo=None):
+    dic = {}
+    if(hasattr(dato, '__dict__')):
+        dic = dato.__dict__
+    if(campo is None or campo not in dic):
+        return dato
+    else:
+        return dic[campo]
+
+
+class nodoLista():
+    info, sig, sublista = None, None, None
+
+
+class Lista():
+
+    def __init__(self):
+        self.__inicio = None
+        self.__tamanio = 0
+
+
+    def insertar(self, dato, campo=None):
+        nodo = nodoLista()
+        nodo.info = dato
+        nodo.sublista = Lista()
+
+        if(self.__inicio is None or criterio(nodo.info, campo) < criterio(self.__inicio.info, campo)):
+            nodo.sig = self.__inicio
+            self.__inicio = nodo
+        else:
+            anterior = self.__inicio
+            actual = self.__inicio.sig
+            while(actual is not None and criterio(nodo.info, campo) > criterio(actual.info, campo)):
+                anterior = anterior.sig
+                actual = actual.sig
+
+            nodo.sig = actual
+            anterior.sig = nodo
+
+        self.__tamanio += 1
+
+    def lista_vacia(self):
+        return self.__inicio is None
+
+    def tamanio(self):
+        return self.__tamanio
+
+    def barrido(self):
+        aux = self.__inicio
+        while(aux is not None):
+            print(aux.info)
+            aux = aux.sig
+    
+    def barrido_lista_lista(self):
+        aux = self.__inicio
+        while(aux is not None):
+            print(aux.info)
+            print('sublista:')
+            aux.sublista.barrido()
+
+            aux = aux.sig
+    
+    def barrido_segun_palabra(self, palabra):
+        aux = self.__inicio
+        while(aux is not None):
+            if(palabra in aux.info.dato):
+                print(aux.info)
+            aux = aux.sig
+
+    def barrido_segun_menor(self, numero):
+        aux = self.__inicio
+        while(aux is not None):
+            if(aux.info.dato < numero):
+                print(aux.info)
+            aux = aux.sig
+    
+    def barrido_segun_mayor(self, numero):
+        aux = self.__inicio
+        while(aux is not None):
+            if(aux.info.dato > numero):
+                print(aux.info)
+            aux = aux.sig
+
+    def barrido_comienza_con(self, letra):
+        aux = self.__inicio
+        while(aux is not None):
+            if(aux.info.dato[0] in letra):
+                print(aux.info)
+            aux = aux.sig
+
+    def barriduo_sublista_barrido(self):
+        aux = self.__inicio
+        while(aux is not None):
+            aux.sublista.barrido()
+            aux = aux.sig
+    
+    def mayor_de_lista(self, campo):
+        aux = self.__inicio
+        mayor = self.__inicio.info
+        while(aux is not None):
+            if(criterio(aux.info, campo) > criterio(mayor, campo)):
+                mayor = aux.info
+            aux = aux.sig
+        return mayor
+
+    def barrido_pocentaje(self, porciento):
+        aux = self.__inicio
+        while(aux is not None):
+            porcentaje = (aux.info.dato1 + aux.info.dato2)
+            if((aux.info.dato / porcentaje) >= porciento):
+                print(aux.info)
+            aux = aux.sig
+
+    def barrido_con_eliminacion(self, barra):
+        aux = self.__inicio
+        while(aux is not None):
+            if(aux.info.dato == barra):
+                print(aux.info)
+                auxilia = aux.sig
+                aux.eliminar()
+            aux = auxilia
+      
+
+    def busqueda(self, buscado, campo=None):
+        pos = None
+        aux = self.__inicio
+        while(aux is not None and pos is None):
+            if(criterio(aux.info, campo) == buscado):
+                pos = aux
+            aux = aux.sig
+
+        return pos
+
+    def eliminar(self, clave, campo=None):
+        dato = None
+        if(criterio(self.__inicio.info, campo) == clave):
+            dato = self.__inicio.info
+            self.__inicio = self.__inicio.sig
+        else:
+            anterior = self.__inicio
+            actual = self.__inicio.sig
+            while(actual is not None and criterio(actual.info, campo) != clave):
+                anterior = anterior.sig
+                actual = actual.sig
+
+            if(actual is not None):
+                dato = actual.info
+                anterior.sig = actual.sig
+        if dato:
+            self.__tamanio -= 1 
+
+        return dato
+
+    def obtener_elemento(self, indice):
+        if(indice <= self.__tamanio-1 and indice >= 0):
+            aux = self.__inicio
+            for i in range(indice):
+                aux = aux.sig
+            return aux.info            
+        else:
+            return None
+
+    def contar_segun_dato(self, dato):
+        contador = 0
+
+        aux = self.__inicio
+        while(aux is not None):
+            if(aux.info.dato == dato):
+                contador += 1
+            aux = aux.sig
+
+        return contador
+
